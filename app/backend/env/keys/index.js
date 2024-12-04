e0bc1dd967136a24cefd3989b4c676313e39dac6
Mathis Botteau <123393731+Froglaprod@users.noreply.github.com>
2024-12-04 16:19:24 +0100

    refactor(path): Changement des chemins en chemin relatifs

--- app/backend/env/keys/index.js
+++ app/backend/env/keys/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 module.exports = {
-    key: fs.readFileSync('C:/Users/po66qga/Documents/GitHub/P_DevOps324-450/app/backend/env/keys/jwtRS256.key'),
-    keyPub: fs.readFileSync('C:/Users/po66qga/Documents/GitHub/P_DevOps324-450/app/backend/env/keys/jwtRS256.key.pub'),
+    key: fs.readFileSync('env/keys/jwtRS256.key'),
+    keyPub: fs.readFileSync('env/keys/jwtRS256.key.pub'),
 }
